d656913316f7bbee1db8550db314efee97d23270
Igo Pinheiro <igopinheiro1@gmail.com>
2016-02-19 15:13:20 -0400

    1. Usercontrol BotaoTelaPrincipal criado - 2. ResourceDictionary implementado no App.xaml

---
+++ wpf-wakusese/wpf-wakusese/src/test/UserControls/BotaoTelaPrincipal.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_wakusese.src.test.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BotaoTelaPrincipal.xaml
+    /// </summary>
+    public partial class BotaoTelaPrincipal : UserControl
+    {
+        public string Titulo
+        {
+            get { return (string)GetValue(TituloProperty); }
+            set { SetValue(TituloProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Titulo.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TituloProperty =
+            DependencyProperty.Register("Titulo", typeof(string), typeof(BotaoTelaPrincipal), new PropertyMetadata("No Title"));
+
+
+
+        public ImageSource Imagem
+        {
+            get { return (ImageSource)GetValue(ImagemProperty); }
+            set { SetValue(ImagemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Imagem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImagemProperty =
+            DependencyProperty.Register("Imagem", typeof(ImageSource), typeof(BotaoTelaPrincipal), new PropertyMetadata(null));
+
+        public event EventHandler Click;
+
+        public BotaoTelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Click != null)
+            {
+                Click(sender, e);
+            }
+
+        }
+    }
+}
